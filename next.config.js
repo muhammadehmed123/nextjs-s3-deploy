0136d24460a4ee9651c2c3c0dd395908f36eac4f
muhammadehmed123 <ahmadsertaj876@gmail.com>
2025-07-28 21:17:17 +0500

    Add files via upload

---
+++ next.config.js
@@ -0,0 +1,23 @@
+// console.log("✅ Using correct next.config.js............................................");
+
+// const nextConfig = {
+//   output: 'export',
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+
+
+// module.exports = nextConfig;
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
