56e3f4f8daa37138f2f0d512700e1f3417387a52
muhammadehmed123 <ahmadsertaj876@gmail.com.com>
2025-07-28 22:10:35 +0500

    updated config of next

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  output: 'export', // <== THIS is required for static export in App Router
   images: {
     unoptimized: true,
   },
