d2c611cad4578a92ed3acafe33ce4914c2784183
muhammadehmed123 <ahmadsertaj876@gmail.com>
2025-07-28 18:39:55 +0500

    Update and rename next.config.mjs to next.config.js

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true, // 👈 disables Image Optimization for static export
-  },
-};
-
-export default nextConfig;
