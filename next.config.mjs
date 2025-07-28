6339caa4de5a0073af9f54e3de20bb8a186fe935
muhammadehmed123 <ahmadsertaj876@gmail.com>
2025-07-28 18:35:02 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: {
+    unoptimized: true, // 👈 disables Image Optimization for static export
+  },
 };
 
 export default nextConfig;
