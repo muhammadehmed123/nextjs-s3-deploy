79b7511d823714f57c55ee0958b0fd4af66a0265
muhammadehmed123 <ahmadsertaj876@gmail.com>
2025-07-31 14:45:48 +0500

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // ✅ required for static export
+  output: 'export',
   images: {
     unoptimized: true,
     remotePatterns: [
