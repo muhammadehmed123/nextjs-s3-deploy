0c66961b41f268bda2a111021b65be093a7359ee
muhammadehmed123 <ahmadsertaj876@gmail.com>
2025-07-31 14:51:21 +0500

    Update postcss.config.mjs

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,8 +1,6 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-  },
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export'
 };
 
-export default config;
+module.exports = nextConfig;
